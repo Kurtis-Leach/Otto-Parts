0a1e2ab1935284ae01217e9fd70a9b319505306e
BobNarwhalKing <danceflurry4366@gmail.com>
2019-08-15 16:49:33 -0500

    css tweaks

--- Client/parts_store/src/components/NavBar.js
+++ Client/parts_store/src/components/NavBar.js
@@ -3,19 +3,20 @@ import { List, Segment, Input, Image } from 'semantic-ui-react'
 import '../assets/NavBar.css'
 import logo from '../assets/parts_pricing_icon.png'
 
+
 export default function NavBar () {
                 // <List.Item className='listItem'><a className='nav-a' href="#news">News</a></List.Item>  New button 
     return(
-        <Segment>
+        <Segment basic inverted className='mainDiv' >
             <List className='list'>
                 <List.Item className='listItem home'>
                         <a className="active nav-a" href="/">
                             <Image src={logo} size='small'/>
                         </a>
                     </List.Item>
-                <List.Item className='listItem'>
-                    <Segment className='searchDiv'>
-                        <Input action='Search' placholder='Search...'/>
+                <List.Item className='searchListItem'>
+                    <Segment  basic className='searchDiv'>
+                        <Input className='inputSearch' action='Search' placholder='Search...'/>
                     </Segment>
                 </List.Item>
             </List>
